7239ee2181223a8e12e4ee31388f424e2c9b8d40
Nick <nickthompso@gmail.com>
2019-08-13 18:08:57 +1200

    Fix Mistakes

--- Tutorial_2/ros_node_boiler_plate.cpp
+++ Tutorial_2/ros_node_boiler_plate.cpp
@@ -6,7 +6,7 @@ void callBack (const /*msg_type& msg*/){
 }
 
 
-int main(int argc, int** argv){
+int main(int argc, char** argv){
 	  /**
    * The ros::init() function needs to see argc and argv so that it can perform
    * any ROS arguments and name remapping that were provided at the command line.
@@ -17,7 +17,7 @@ int main(int argc, int** argv){
    * You must call one of the versions of ros::init() before using any other
    * part of the ROS system.
    */
-	ros::init(argc,argv, "**NODE NAME**")
+	ros::init(argc,argv, "**NODE NAME**");
 	
 	/**
    * NodeHandle is the main access point to communications with the ROS system.
